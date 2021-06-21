1cfe7fd5948794fd96aed5541f23c1b2103fda29
Farhan Masud Aneek <farhan.aneek@gmail.com>
2021-06-21 17:16:12 +0600

    Basic template

---
+++ config/settings/engage.py
@@ -0,0 +1,11 @@
+from .base import *
+
+working_environment = env.str("WORK_ENV", default="local")
+
+
+if working_environment == "production":
+    from .production import *
+elif working_environment == "test":
+    from .test import *
+else:
+    from .local import *
---
+++ config/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Test API",
+        default_version="v1",
+        description="Test API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="test@test.com"),
+        license=openapi.License(name="BSD License"),
+        url="/",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("auth/", include("dj_rest_auth.urls")),
+    path("auth/registration/", include("dj_rest_auth.registration.urls")),
+    path(
+        "swagger-api/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
